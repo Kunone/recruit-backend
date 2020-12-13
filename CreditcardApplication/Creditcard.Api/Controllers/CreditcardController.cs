df41cdb667746acdcab777eded1a1b5b303e0613
kun <kun@kunone.localdomain>
2020-12-13 15:28:41 +1300

    feature/add-controller-and-rest-api-test

---
+++ CreditcardApplication/Creditcard.Api/Controllers/CreditcardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creditcard.Api.Controllers
+{
+    [ApiController]
+    [Route("api/credit-cards")]
+    public class CreditcardController : ControllerBase
+    {
+        public CreditcardController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok("hello world");
+        }
+    }
+}
