2ccd73305c0c54ec58c391079833ac4f165be13f
kun <kun@kunone.localdomain>
2020-12-13 15:19:51 +1300

    feature/add-creditcard-model-and-init-database

---
+++ CreditcardApplication/Creditcard.DataContract/Creditcard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Creditcard.DataContract
+{
+    public class Creditcard
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string CardNumber { get; set; } // to be encrypted
+        public string CVC { get; set; } // to be encrypted
+        public DateTime ExpiryDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ SQL/init.sql
@@ -0,0 +1,25 @@
+USE [Recruit-Backend]
+GO
+
+CREATE SCHEMA Customer
+Go
+
+
+CREATE TABLE Customer.Profile
+(
+    Id uniqueidentifier NOT NULL Default NEWID(),
+    FirstName varchar(100) NOT NULL,
+	LastName varchar(100) NOT NULL
+);
+Go
+
+create TABLE Customer.Card
+(
+    Id uniqueidentifier NOT NULL Default NEWID(),
+    UserId uniqueidentifier NOT NULL,
+    Name varchar(50) NOT NULL,
+	CardNumber varchar(100) NOT NULL,
+	CVC varchar(50) NOT NULL,
+	ExpiryDate DATE NOT NULL
+);
+Go
